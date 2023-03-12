072aff39342678027a64a971676ca480bbf93509
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-03-11 17:20:26 -0700

    Added helmet js and updated prod config

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const compression = require('compression')
-require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const compression = require("compression");
+const helmet = require("helmet");
+require("dotenv").config();
 
-const { helloRoutes, apiRoutes } = require('./routes')
+const { helloRoutes, apiRoutes } = require("./routes");
 
-const app = express()
+const app = express();
 
 // middleware
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(compression()) // compress responses to enable server-sent events
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression()); // compress responses to enable server-sent events
+app.use(helmet());
 
 // routes
-app.use('/', helloRoutes)
-app.use('/api', apiRoutes)
+app.use("/", helloRoutes);
+app.use("/api", apiRoutes);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(
-    `Upload Events service listening at ${process.env.SERVER_URL}${PORT}`
-  )
-})
+	console.log(
+		`Upload Events service listening at ${process.env.SERVER_URL}${PORT}`
+	);
+});
